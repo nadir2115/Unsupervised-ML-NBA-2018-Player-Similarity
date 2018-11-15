25bf1f4477ed00ce75f3a4de5ac6d39b10b06e77
Nadir Nibras <32527014+nadir2115@users.noreply.github.com>
2018-11-14 23:33:36 -0800

    Cleaned comments and variable names

--- Player analysis.R
+++ Player analysis.R
@@ -1,14 +1,16 @@
-#Unsupervised learning on NBA players- Nadir Nibras
+#Unsupervised Machine Learning project to study NBA player similarity- Nadir Nibras
 
-rm(list = ls()); # clear workspace variables
-cat("\014")   # it means ctrl+L. clear window
-graphics.off() # close all plots
+# clear workspace variables
+rm(list = ls()); 
+# clear window (same as ctrl+L. )
+cat("\014")   
+# close all plots
+graphics.off() 
 
 
 library(dplyr)
 library(cluster)
 library(factoextra)
-library(fpc)
 library(ggplot2)
 library(readr)
 library(datasets)
@@ -20,110 +22,113 @@ library(BBmisc)
 
 # load data ---------------------------------------------------------------
 
-trad<-read_csv("C:/Users/nadir/Documents/Machine Learning/NBA/36 minutes.csv")
-advanced<- read_csv("C:/Users/nadir/Documents/Machine Learning/NBA/advanced.csv")
-extra<- read_csv("C:/Users/nadir/Documents/Machine Learning/NBA/extra.csv")
+traditionalStats<-read.csv("C:/Users/nadir/Documents/Machine Learning/NBA/36 minutes.csv")
+advancedStats<- read.csv("C:/Users/nadir/Documents/Machine Learning/NBA/advanced.csv")
+miscStats<- read.csv("C:/Users/nadir/Documents/Machine Learning/NBA/extra.csv")
 
 # Data pre-processing -----------------------------------------------------
 
-#merging 1st 2 datasets
-data<-merge(trad,advanced);                      
+# Merge 1st 2 datasets
+mergedData<-merge(traditionalStats,advancedStats);                      
 
-#Fixing mismatches for merging
-data$Player=gsub(".", "", data$Player, fixed = TRUE); 
-extra$Player=gsub(".", "", extra$Player, fixed = TRUE);
-extra$Player=gsub(" Jr", "", extra$Player, fixed = TRUE);
-extra$Player=gsub(" III", "", extra$Player, fixed = TRUE);
-extra$Player=gsub(" IV", "", extra$Player, fixed = TRUE);
-extra$Player=gsub(" II", "", extra$Player, fixed = TRUE);
-data$Player=gsub("Walt Lemon Jr", "Walter Lemon", data$Player, fixed = TRUE);
-data$Player=gsub("Vince Hunter", "Vincent Hunter", data$Player, fixed = TRUE);
-data$Player=gsub("Nene Hilario", "Nene", data$Player, fixed = TRUE);
-data$Player=gsub("Taurean Waller-Prince", "Taurean Prince", data$Player, fixed = TRUE);
-data$Player=gsub("Wesley Iwundu", "Wes Iwundu", data$Player, fixed = TRUE);
+# Fix mismatches for merging
+mergedData$Player=gsub(".", "", mergedData$Player, fixed = TRUE); 
+miscStats$Player=gsub(".", "", miscStats$Player, fixed = TRUE);
+miscStats$Player=gsub(" Jr", "", miscStats$Player, fixed = TRUE);
+miscStats$Player=gsub(" III", "", miscStats$Player, fixed = TRUE);
+miscStats$Player=gsub(" IV", "", miscStats$Player, fixed = TRUE);
+miscStats$Player=gsub(" II", "", miscStats$Player, fixed = TRUE);
+mergedData$Player=gsub("Walt Lemon Jr", "Walter Lemon", mergedData$Player, fixed = TRUE);
+mergedData$Player=gsub("Vince Hunter", "Vincent Hunter", mergedData$Player, fixed = TRUE);
+mergedData$Player=gsub("Nene Hilario", "Nene", mergedData$Player, fixed = TRUE);
+mergedData$Player=gsub("Taurean Waller-Prince", "Taurean Prince", mergedData$Player, fixed = TRUE);
+mergedData$Player=gsub("Wesley Iwundu", "Wes Iwundu", mergedData$Player, fixed = TRUE);
 
-#Checking for number of mis-matches
-data$Player[!(data$Player %in% extra$Player)]
-extra$Player[!(extra$Player %in% data$Player)]
+# Check for number of mis-matches
+mergedData$Player[!(mergedData$Player %in% miscStats$Player)]
+miscStats$Player[!(miscStats$Player %in% mergedData$Player)]
 
-#merging final misc datasets
-data<-merge(data,extra); 
+# Merge misc dataset
+mergedData<-merge(mergedData,miscStats); 
 
-#removing redundant columns
-data<- data[c(1,3:49,54:57,62)] 
+# Remove redundant columns
+mergedData<- mergedData[c(1,3:49,54:57,62)] 
 
-#Fature extraction
-data$MPG<-data$MP/data$G;
-data$A2TO<-data$AST/data$TOV
+# Feature extraction
+mergedData$MPG<-mergedData$MP/mergedData$G;
+mergedData$A2TO<-mergedData$AST/mergedData$TOV
 
-#removing null values
-data[is.na(data)] <- 0 
+# Remove null values
+mergedData[is.na(mergedData)] <- 0 
 
-#Selecting threshold for subsetting
+# Select threshold for subsetting
 cutoff<-28.5 
 
-#subsetting data using MPG
-newdata <- subset(data, MPG >=cutoff );
-impdata<- newdata[c(9:55)];
-# impdata<- newdata[c(8:20,23,26,28,30:33, 36, 39:41, 45, 49:53,55)]; #offense features
-# impdata<- newdata[c(21,24,25,27,34,37,38,42,46)]; #defense features
+# Subset data using Minutes per game
+mergedData <- subset(mergedData, MPG >=cutoff );
 
-#normalizing data
-impdata <- normalize(impdata, method= "standardize") 
+# Subset Daraframe using relevant features
+mainData<- mergedData[c(9:55)]; #relevant overall features
+# mergedData<- mergedData[c(8:20,23,26,28,30:33, 36, 39:41, 45, 49:53,55)]; #offense features
+# mergedData<- mergedData[c(21,24,25,27,34,37,38,42,46)]; #defense features
 
-#Correlation plot
-o=corrplot(cor(impdata),method='number')
+# Normalize data
+mainData <- normalize(mainData, method= "standardize") 
+
+# Feature Correlation plot
+o=corrplot(cor(mainData),method='number')
 
 # PCA ---------------------------------------------------------------------
 
-res.cov <- cov(impdata);
+res.cov <- cov(mainData);
 round(res.cov,2);
 
-# find eigenvectors
+# Find eigenvectors
 eig<-eigen(res.cov)$vectors;
 
-# select 1st 2 eigenvectors
-pc2<-eig[,(1:2)];
+# Select 1st 2 eigenvectors
+eigenv2<-eig[,(1:2)];
 
-# converting table to matrix
-datamat<-data.matrix(impdata, rownames.force = NA);
+# Convert to matrix for PCA calculations
+statsMatrix<-data.matrix(mainData, rownames.force = NA);
 
-# matrix multiplication
-pcad2<-datamat%*%pc2;
+# Matrix multiplication
+PrincipalComponents2<-statsMatrix%*%eigenv2;
 
-# convert back to dataframe for ggplot
-dataFrame<-data.frame(pcad2); 
+# Convert back to statsDF for ggplot
+statsDF<-data.frame(PrincipalComponents2); 
 
 # PCA Plot
-ggplot(dataFrame,aes(dataFrame$X1, dataFrame$X2)) +
+ggplot(statsDF,aes(statsDF$X1, statsDF$X2)) +
   labs(x="PC1",y="PC2")+
-  geom_point(data=newdata,aes(col =Pos, size= VORP))+
-  geom_text_repel(data=newdata, aes(label=Player), size=3+newdata$VORP/max(newdata$VORP))
+  geom_point(data=mergedData,aes(col =Pos, size= VORP))+
+  geom_text_repel(data=mergedData, aes(label=Player), size=3+mergedData$VORP/max(mergedData$VORP))
 
 # k-means clustering ---------------------------------------------------
 
-# Replacing index with player name (used for plot)
-row.names(impdata) <- newdata$Player 
+# Replace index with player name (used for plot)
+row.names(mainData) <- mergedData$Player 
 
 # Clarify distance measures
-res.dist <- get_dist(impdata, stand = TRUE, method = "euclidean")
-fviz_dist(res.dist, 
-          gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+res.dist <- get_dist(mainData, stand = TRUE, method = "euclidean")
+fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
 
 # Determinie optimal clusters through different methods
-fviz_nbclust(impdata, kmeans, method = "wss")
-fviz_nbclust(impdata, kmeans, method = "silhouette")
-fviz_nbclust(impdata, kmeans, method = "gap_stat")
+fviz_nbclust(mainData, kmeans, method = "wss")
+fviz_nbclust(mainData, kmeans, method = "silhouette")
+fviz_nbclust(mainData, kmeans, method = "gap_stat")
 
 # Kmeans based on best cluster-number
-km.res <- kmeans(impdata,8, nstart = 25)
+km.res <- kmeans(mainData,8, nstart = 25)
 
-# Visualize in clusters
-fviz_cluster(km.res, data =impdata, ellipse = TRUE, ellipse.alpha= 0.1,
+# Visualize Kmeans clusters
+fviz_cluster(km.res, mainData, ellipse = TRUE, ellipse.alpha= 0.1,
              palette = "jco",repel = TRUE, ggtheme = theme_minimal(), 
              main= FALSE, xlab= FALSE, ylab = FALSE)
-Clusters=data.frame(sort(km.res$cluster));
 
+# Further cluster analysis
+Clusters=data.frame(sort(km.res$cluster));
+km.res;
 
 # hierarchial clustering --------------------------------------------------
 
@@ -134,4 +139,3 @@ fviz_dend(res.hc, k = 8, # Cut in 8 groups
           cex = 0.5, # label size
            horiz= TRUE, rect = TRUE # Add rectangle around groups
           )
-
